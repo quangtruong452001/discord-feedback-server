1270f94362b5a0dd27e10feb51102a20f0d8d953
TruongDangQuang <quangtruong452001@gmail.com>
2023-12-15 02:54:22 +0700

    Update: routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/checkstatus', (req, res) => {
   return res.status(200).json({
     status: 'success',
     message: 'Welcome to the API',
